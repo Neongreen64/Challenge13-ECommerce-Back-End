f282b9c9faa7c39b81a70a0aa7464f8359089f0f
neon <dogmanyo@gmail.com>
2023-11-21 14:30:56 -0500

    added category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -25,14 +25,37 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body)
+  .then(createdCategory => res.status(200).json(createdCategory))
+  .catch(e => res.status(400).json(e))
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: { id:req.params.id }
+  })
+  .then(updateCategory => {
+    if(updateCategory[0] === 0){
+      return res.status(400).json({ message: "Can't find category"});
+    }
+    res.status(200).json({ message: "Category updated successfully"});
+  })
+  .catch(e => res.status(400).json(e));
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: { id:req.params.id }
+  })
+  .then(delRow => {
+    if(delRow === 0){
+      return res.status(400).json({ message: "Can't find category"})
+    }
+    res.status(200).json({ message: "Category deleted successfully"});
+  })
+  .catch(e => res.status(400).json(e));
 });
 
 module.exports = router;
